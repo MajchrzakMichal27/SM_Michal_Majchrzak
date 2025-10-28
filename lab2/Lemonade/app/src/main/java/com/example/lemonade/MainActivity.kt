879a7562fcbd8a69a62f426e77b8d2a531e9c9f7
Krzysztof Ząbecki <zk55767@zut.edu.pl>
2025-10-28 15:23:15 +0100

    lemonade done

---
+++ lab2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.lemonade
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                LemonadeApp()
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var random by remember { mutableStateOf(0) }
+    var squeezed by remember { mutableStateOf(0) }
+
+
+    when (currentStep) {
+        1 -> LemonadeTree(onImageClick = {
+            currentStep = 2
+            random = (2..4).random()
+            squeezed = 0
+        })
+
+        2 -> LemondeLemon(onImageClick = {
+            squeezed++
+            if(squeezed>=random){ currentStep = 3}
+
+            })
+
+        3 -> LemondeFullGlass(onImageClick = { currentStep = 4 })
+        4 -> LemondeRestart(onImageClick = { currentStep = 1 })
+    }
+}
+
+@Composable
+fun LemonadeTree(onImageClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Image(
+            painter = painterResource(R.drawable.lemon_tree),
+            contentDescription = "Lemon tree",
+            modifier = Modifier.clickable{onImageClick()}
+        )
+
+            Text(text = stringResource(R.string.LemonTree), fontSize = 24.sp)
+
+    }
+}
+
+@Composable
+fun LemondeLemon(onImageClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.lemon_squeeze),
+            contentDescription = "Squeezing lemon",
+            modifier = Modifier.clickable{onImageClick()}
+
+        )
+            Text(text = stringResource(R.string.LemonSqueeze), fontSize = 24.sp)
+
+    }
+}
+
+@Composable
+fun LemondeFullGlass(onImageClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.lemon_drink),
+            contentDescription = "Full glass",
+            modifier = Modifier.clickable{onImageClick()}
+
+        )
+            Text(text = stringResource(R.string.LemonDrink), fontSize = 24.sp)
+
+    }
+}
+
+@Composable
+fun LemondeRestart(onImageClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.lemon_restart),
+            contentDescription = "Restart",
+            modifier = Modifier.clickable{onImageClick()}
+
+        )
+            Text(text = stringResource(R.string.LemonRestart), fontSize = 24.sp)
+
+    }
+}
